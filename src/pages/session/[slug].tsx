240af3159d0c43ea204014ce23cdd4e01fabcb01
Kevin Dam <kevinqdam@gmail.com>
2023-04-22 17:55:05 -0700

    Implement basic reorder in queue on session page

--- src/pages/session/[slug].tsx
+++ src/pages/session/[slug].tsx
@@ -1,8 +1,38 @@
 import { NextPage } from "next";
-import RootLayout from "../../components/Layout";
+import { api } from "~/utils/api";
+import { Reorder } from "framer-motion";
+import { useState } from "react";
 
 const Session: NextPage = () => {
-  return <div>hello</div>
+  const sessionUsers = api.example.sessionUsers.useQuery({ slug: "123" });
+  const [items, setItems] = useState(["foo", "bar", "baz"]);
+
+  return (
+    <main className="flex flex-row justify-evenly gap-2">
+      <div className="flex w-full flex-col">
+        <h1 className="text-center">In the queue</h1>
+        <div className="h-64 w-full bg-red-200 p-2">
+          <Reorder.Group values={items} onReorder={setItems}>
+            {items.map((item) => (
+              <Reorder.Item key={item} value={item}>
+                {item}
+              </Reorder.Item>
+            ))}
+          </Reorder.Group>
+        </div>
+      </div>
+      <div className="w-1 bg-slate-300"></div>
+      <div className="flex w-full flex-col">
+        <h1 className="text-center">Up next</h1>
+        <div className="h-64 w-full bg-green-200"></div>
+      </div>
+      <div className="w-1 bg-slate-300"></div>
+      <div className="flex w-full flex-col">
+        <h1 className="text-center">Went already</h1>
+        <div className="h-64 w-full bg-blue-200"></div>
+      </div>
+    </main>
+  );
 };
 
 export default Session;
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+type List = "QUEUE" | "NEXT" | "WENT";
+
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
@@ -10,7 +12,21 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  sessionUsers: publicProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(async ({ input }) => {
+      // TODO: use Prisma client to fetch users for the session by slug
+
+      // simulate network delay
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+
+      const users = [
+        { name: "Foo", list: "QUEUE" },
+        { name: "Bar", list: "QUEUE" },
+        { name: "Baz", list: "QUEUE" },
+        { name: "Qux", list: "QUEUE" },
+      ];
+
+      return Promise.resolve(users);
+    }),
 });
