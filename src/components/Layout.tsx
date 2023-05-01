162446d3039b6e0ed0b339b17bf725265ce7695a
Kevin Dam <kevinqdam@gmail.com>
2023-04-30 22:07:57 -0700

    Transition on navbar buttons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,10 +9,10 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
           <div>[Logo] Is it my turn?</div>
         </div>
         <div className="flex flex-row gap-6">
-          <button className="rounded-lg bg-gray-300 px-4 py-2 hover:bg-gray-400">
+          <button className="rounded-lg bg-gray-300 px-4 py-2 transition hover:bg-gray-400">
             Open
           </button>
-          <button className="rounded-lg bg-teal-500 px-4 py-2 text-white hover:bg-teal-700">
+          <button className="rounded-lg bg-teal-500 px-4 py-2 text-white transition hover:bg-teal-700">
             Create a session
           </button>
         </div>
