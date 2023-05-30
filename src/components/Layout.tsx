9a1217444234b43659a7fe9ce96f7a50a589da09
Kevin Dam <kevinqdam@gmail.com>
2023-05-29 23:50:43 -0700

    Remove navbar buttons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,14 +7,7 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
     <div className="flex h-screen flex-col justify-between">
       <nav className="flex flex-row items-center justify-between px-12 py-6 text-xl">
         <Logo />
-        <div className="flex flex-row gap-6">
-          <button className="rounded-lg bg-gray-300 px-4 py-2 transition hover:bg-gray-400">
-            Open
-          </button>
-          <button className="rounded-lg bg-teal-500 px-4 py-2 text-white transition hover:bg-teal-700">
-            Create a session
-          </button>
-        </div>
+        <div />
       </nav>
       <LayoutGroup>
         {children}
