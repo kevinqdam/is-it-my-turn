36e6234a5051614b738285d266fa17b0187c4160
Kevin Dam <kevinqdam@gmail.com>
2023-05-31 00:06:38 -0700

    Extract icon

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,6 +7,23 @@ import appDemo from "../../../public/is-it-my-turn.gif";
 
 export const MAX_SESSION_NAME_INPUT_LENGTH = 50;
 
+const ArrowTopRightOnSquareIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="h-6 w-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+    />
+  </svg>
+);
+
 const About: NextPage = () => {
   return (
     <>
@@ -68,20 +85,7 @@ const About: NextPage = () => {
                 rel="noopener noreferrer"
               >
                 Visit my Ko-fi page
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                  />
-                </svg>
+                <ArrowTopRightOnSquareIcon />
               </Link>
             </div>
           </div>
