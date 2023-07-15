02765b5501fa2f0e6981bc7c8be340700c338d3d
Kevin Dam <kevinqdam@gmail.com>
2023-07-15 15:27:27 -0700

    Implement returning to session and reduce layout shift on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { messageFromError, toSessionSlug } from "~/utils/session-name";
 import Spinner from "~/components/Spinner";
 import { useRouter } from "next/router";
 import { useDebouncedCallback } from "use-debounce";
+import Link from "next/link";
 
 export const MAX_SESSION_NAME_INPUT_LENGTH = 50;
 
@@ -150,147 +151,160 @@ const Home: NextPage = () => {
               <span className="text-teal-500">Without replacement.</span>
             </div>
             <div className="flex flex-col items-center gap-16 lg:flex-row lg:justify-evenly">
-              <div className="flex flex-col gap-y-4 px-16">
-                <span className="text-4xl font-semibold">Perfect for...</span>
-                <ul className="flex flex-col gap-2 text-2xl">
-                  <li>
-                    <span className="flex flex-row items-center gap-4">
-                      <div className="text-4xl">🎲</div>
-                      <div className="text-gray-700">Board game night</div>
-                    </span>
-                  </li>
-                  <li>
-                    <span className="flex flex-row items-center gap-4">
-                      <div className="text-4xl">🍱</div>
-                      <div className="text-gray-700">Meal planning</div>
-                    </span>
-                  </li>
-                  <li>
-                    <span className="flex flex-row items-center gap-4">
-                      <div className="text-4xl">🧹</div>
-                      <div className="text-gray-700">Chore duty</div>
-                    </span>
-                  </li>
-                  <li>
-                    <span className="flex flex-row items-center gap-4">
-                      <div className="text-4xl">🗣️</div>
-                      <div className="text-gray-700">
-                        Rotating stand-up leaders
-                      </div>
-                    </span>
-                  </li>
-                  <li className="pt-4 italic text-gray-700">...and more!</li>
-                </ul>
+              <div className="flex flex-col justify-start">
+                <div className="flex flex-col gap-y-4 px-16">
+                  <span className="text-4xl font-semibold">Perfect for...</span>
+                  <ul className="flex flex-col gap-2 text-2xl">
+                    <li>
+                      <span className="flex flex-row items-center gap-4">
+                        <div className="text-4xl">🎲</div>
+                        <div className="text-gray-700">Board game night</div>
+                      </span>
+                    </li>
+                    <li>
+                      <span className="flex flex-row items-center gap-4">
+                        <div className="text-4xl">🍱</div>
+                        <div className="text-gray-700">Meal planning</div>
+                      </span>
+                    </li>
+                    <li>
+                      <span className="flex flex-row items-center gap-4">
+                        <div className="text-4xl">🧹</div>
+                        <div className="text-gray-700">Chore duty</div>
+                      </span>
+                    </li>
+                    <li>
+                      <span className="flex flex-row items-center gap-4">
+                        <div className="text-4xl">🗣️</div>
+                        <div className="text-gray-700">
+                          Rotating stand-up leaders
+                        </div>
+                      </span>
+                    </li>
+                    <li className="pt-4 italic text-gray-700">...and more!</li>
+                  </ul>
+                </div>
+                {/* Placeholder width for non-mobile viewports */}
+                <div className="md:h-[50px]" />
               </div>
-              <div className="mx-8">
-                <div className="flex w-full flex-col items-center justify-center gap-8">
-                  <input
-                    type="text"
-                    className="w-64 rounded-lg border border-gray-300 bg-gray-50 px-4 py-2 text-lg text-gray-900 focus:border-teal-500 focus:outline-none focus:ring-teal-500"
-                    placeholder="Enter your session name"
-                    onInput={handleOnInput}
-                    required
-                  />
-                  {
-                    <div className="flex w-full grow-0 flex-col items-center gap-2">
-                      <div className="flex flex-row items-center justify-center gap-2">
-                        <strong
+              <div className="mx-8 flex h-full min-w-fit flex-col items-center gap-8 md:w-[522px]">
+                <input
+                  type="text"
+                  className="w-64 rounded-lg border border-gray-300 bg-gray-50 px-4 py-2 text-lg text-gray-900 focus:border-teal-500 focus:outline-none focus:ring-teal-500"
+                  placeholder="Enter your session name"
+                  onInput={handleOnInput}
+                  required
+                />
+                {newSlugDisplayedToClient && (
+                  <div className="flex w-full grow-0 flex-col items-center gap-2">
+                    <div className="flex flex-row items-center justify-center gap-2">
+                      <strong
+                        className={cn(
+                          "whitespace-nowrap",
+                          shouldShowError ? "text-red-500" : "text-gray-600"
+                        )}
+                      >
+                        Your session slug will be:
+                      </strong>
+                      {newSlugDisplayedToClient && (
+                        <code
                           className={cn(
-                            "whitespace-nowrap",
-                            shouldShowError ? "text-red-500" : "text-gray-600"
+                            "w-full max-w-xs grow-0 overflow-auto rounded-md px-1.5 py-1",
+                            shouldShowError
+                              ? "bg-pink-100 text-red-500"
+                              : "bg-gray-200 text-gray-600"
                           )}
                         >
-                          Your session slug will be:
-                        </strong>
-                        {newSlugDisplayedToClient && (
-                          <code
-                            className={cn(
-                              "grow-0 w-full max-w-xs overflow-auto rounded-md px-1.5 py-1",
-                              shouldShowError
-                                ? "bg-pink-100 text-red-500"
-                                : "bg-gray-200 text-gray-600"
-                            )}
-                          >
-                            {newSlugDisplayedToClient}
-                          </code>
-                        )}
-                      </div>
-                    </div>
-                  }
-                  {createSessionMutation.isIdle && (
-                    <div className="flex w-full flex-col items-center justify-center gap-8">
-                      <div className="flex flex-row justify-evenly">
-                        <button
-                          disabled={isButtonDisabled}
-                          onClick={handleOnClickGetStarted}
-                          className="rounded-lg border bg-teal-500 px-5 py-3 text-center text-4xl text-white transition hover:bg-teal-700 disabled:bg-gray-400 disabled:hover:cursor-not-allowed"
-                        >
-                          Get started
-                        </button>
-                      </div>
-                      {shouldShowError && (
-                        <ul className="flex flex-col gap-2 px-2 text-red-500 lg:w-3/5 lg:px-0">
-                          {sessionNameErrors.map((errorType) => {
-                            const { primaryMessage, secondaryMessage } =
-                              messageFromError(errorType);
-                            return (
-                              <li key={errorType}>
-                                <div
-                                  className="border-l-4 border-red-500 bg-red-100 p-4 text-red-700"
-                                  role="alert"
-                                >
-                                  <p className="font-bold">{primaryMessage}</p>
-                                  <p>{secondaryMessage}</p>
-                                </div>
-                              </li>
-                            );
-                          })}
-                        </ul>
+                          {newSlugDisplayedToClient}
+                        </code>
                       )}
-                      {(isTyping && <Spinner />) ||
-                        (hasProvidedInput && slugExistsQuery.isLoading && (
-                          <Spinner />
-                        ))}
-                      {shouldShowSlugAvailableMessage && (
+                    </div>
+                  </div>
+                )}
+                {createSessionMutation.isIdle && (
+                  <div className="flex w-full flex-col items-center justify-center gap-8">
+                    <div className="flex flex-row justify-evenly">
+                      <button
+                        disabled={isButtonDisabled}
+                        onClick={handleOnClickGetStarted}
+                        className="rounded-lg border bg-teal-500 px-5 py-3 text-center text-4xl text-white transition hover:bg-teal-700 disabled:bg-gray-400 disabled:hover:cursor-not-allowed"
+                      >
+                        Get started
+                      </button>
+                    </div>
+                    {shouldShowError && (
+                      <ul className="flex flex-col gap-2 px-2 text-red-500 md:w-[522px] lg:px-0">
+                        {sessionNameErrors.map((errorType) => {
+                          const { primaryMessage, secondaryMessage } =
+                            messageFromError(errorType);
+                          return (
+                            <li key={errorType}>
+                              <div
+                                className="border-l-4 border-red-500 bg-red-100 p-4 text-red-700"
+                                role="alert"
+                              >
+                                <p className="font-bold">{primaryMessage}</p>
+                                <p>{secondaryMessage}</p>
+                              </div>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    )}
+                    {(isTyping && <Spinner />) ||
+                      (hasProvidedInput && slugExistsQuery.isLoading && (
+                        <Spinner />
+                      ))}
+                    {shouldShowSlugAvailableMessage && (
+                      <div
+                        className="border-l-4 border-teal-500 bg-teal-100 p-4 text-teal-700"
+                        role="alert"
+                      >
+                        <p className="font-bold">The slug is available!</p>
+                        <p>
+                          Click <em>Get started</em> to begin your session!
+                        </p>
+                      </div>
+                    )}
+                    {shouldShowSlugTakenMessage && (
+                      <div className="max-w-[522px] px-4">
                         <div
-                          className="border-l-4 border-teal-500 bg-teal-100 p-4 text-teal-700"
+                          className="border-l-4 border-yellow-500 bg-yellow-100 p-4 text-yellow-700"
                           role="alert"
                         >
-                          <p className="font-bold">The slug is available!</p>
+                          <p className="font-bold">
+                            That slug is already being used for another session.
+                          </p>
                           <p>
-                            Click <em>Get started</em> to begin your session!
+                            If this slug is yours, click{" "}
+                            <Link
+                              href={`/session/${newSlugToCheckExists}`}
+                              className="text-blue-600 visited:text-purple-600 hover:underline"
+                            >
+                              here
+                            </Link>{" "}
+                            to return to your session.
+                          </p>
+                          <p>
+                            If not, then please try using another session name.
                           </p>
                         </div>
-                      )}
-                      {shouldShowSlugTakenMessage && (
-                        <div className="px-4">
-                          <div
-                            className="border-l-4 border-red-500 bg-red-100 p-4 text-red-700"
-                            role="alert"
-                          >
-                            <p className="font-bold">
-                              Sorry, that slug is already being used for another
-                              session!
-                            </p>
-                            <p>Please try using another session name.</p>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  )}
-                  {createSessionMutation.isLoading && <Spinner />}
-                  {createSessionMutation.isError && (
-                    <div
-                      className="border-l-4 border-red-500 bg-red-100 p-4 text-red-700"
-                      role="alert"
-                    >
-                      <p className="font-bold">
-                        Sorry, something went wrong while creating your session!
-                      </p>
-                      <p>Please refresh the page and try again.</p>
-                    </div>
-                  )}
-                </div>
+                      </div>
+                    )}
+                  </div>
+                )}
+                {createSessionMutation.isLoading && <Spinner />}
+                {createSessionMutation.isError && (
+                  <div
+                    className="border-l-4 border-red-500 bg-red-100 p-4 text-red-700"
+                    role="alert"
+                  >
+                    <p className="font-bold">
+                      Sorry, something went wrong while creating your session!
+                    </p>
+                    <p>Please refresh the page and try again.</p>
+                  </div>
+                )}
               </div>
             </div>
           </div>
